774e27bf319ae99cd0a795da68caea2ae1f91197
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-11-09 13:42:48 +0100

    add submit title+icon

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,22 @@
-import styles from './List.module.scss'
-import Column from '../Column/Column'
+import styles from './List.module.scss';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
+
+  const [columns, setColumns] = useState([
+    { id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ]);
+
+
+  const addColumn = newColumn => {
+    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
+  };
+
   return (
     <div className={styles.list}>
       <header className={styles.header}>
@@ -9,10 +24,14 @@ const List = () => {
       </header>
       <p className={styles.description}>“Interesting things I want to check out”</p>
       <section className={styles.columns}>
-        <Column icon="book" title="Books"/>
-        <Column icon="film" title="Movies"/>
-        <Column icon="gamepad" title="Games"/>
+        {/*PĘTLA... przejdź po tablicy z obiektami kolumn i zwróć nową, gdzie każdy obiekt będzie zastąpiony elementem <Column> z odpowiednimi parametrami. 
+        -column to jeden obiekt wybrany z tablicy 
+        -key ma być unikalnym identyfikatorem dla reacta- zawsze należy go nadawać kiedy mapujemy po tablicy!!! */}
+        {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
       </section>
+      {/*onSubmit - dodanie do elemetu(submit) nasłuchiwacza w klamerkach jest nazwa funkcji callback przy wykryciu eventu*/}
+
+      <ColumnForm action={addColumn} />
     </div>
   );
 };
