a15699f4d1584d283ecbb5db54cd5582f4c3d451
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-11-14 21:43:13 +0100

    add access to data from the central database

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,10 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card'
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = (props) => {
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
     return (
         <article className={styles.column}>
 
@@ -10,7 +12,7 @@ const Column = (props) => {
                 <span className={styles.icon + ' fa fa-' + props.icon} />
                 {props.title}</h2>
             <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                {cards.map(card => <Card key={card.id} title={card.title} />)}
             </ul>
             <CardForm columnId={props.id} action={props.columnId} addCard={props.addCard} />
         </article>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import ReactDOM from 'react-dom';
+
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+  </Provider>
+  </React.StrictMode>,
+)
+
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './styles/normalize.scss';
+// import './styles/global.scss';
+// import 'font-awesome/css/font-awesome.min.css';
+
+// ReactDOM.render(
+//   <App />,
+//   document.querySelector('#root')
+// );
